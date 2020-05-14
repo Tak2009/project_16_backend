2a528d7694819897224acd3690c7fd66f6e1cbcc
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-14 17:02:31 +0100

    r g r ticket pub:references supporter:references

---
+++ db/migrate/20200514160146_create_tickets.rb
@@ -0,0 +1,10 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.references :pub, null: false, foreign_key: true
+      t.references :supporter, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
