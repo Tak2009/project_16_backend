797df62eb4135dbfdd5631a7ed9fe894987fcba5
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-14 17:00:41 +0100

    rails g resource supporter name address postcode

---
+++ db/migrate/20200514160034_create_supporters.rb
@@ -0,0 +1,11 @@
+class CreateSupporters < ActiveRecord::Migration[6.0]
+  def change
+    create_table :supporters do |t|
+      t.string :name
+      t.string :address
+      t.string :postcode
+
+      t.timestamps
+    end
+  end
+end
