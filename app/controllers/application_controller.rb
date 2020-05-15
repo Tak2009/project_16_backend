b2cbe4f27ae22b824662850ccabd38ec6f0e1c5b
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-15 11:34:23 +0100

    chnaged

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
 
     def basic
         # プロダクション環境だけBASIC認証をかけたい
-        # if Rails.env == "production"
+        # if Rails.env.production? 
         http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
     end
 
