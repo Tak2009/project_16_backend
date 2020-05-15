967d2447579b24b53d36794bce612c368a470a50
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-15 20:48:33 +0100

    Update application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,12 @@
 class ApplicationController < ActionController::API
-    before_action :basic
+    # before_action :basic
       
-    private
+    # private
 
-    def basic
-        # プロダクション環境だけBASIC認証をかけたい
-        # if Rails.env.production? 
-        http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
-    end
+    # def basic
+    #     # プロダクション環境だけBASIC認証をかけたい
+    #     # if Rails.env.production? 
+    #     http_basic_authenticate_with name: ENV['BASIC_AUTH_NAME'], password: ENV['BASIC_AUTH_PASSWORD'] if Rails.env.production?
+    # end
 
 end
