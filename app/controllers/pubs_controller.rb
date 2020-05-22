5cc8697458422315c654f9669e54989887c5ad6b
Takeshi Kimoto <lnuk2009jp@gmail.com>
2020-05-22 23:40:21 +0100

    devise

--- app/controllers/pubs_controller.rb
+++ app/controllers/pubs_controller.rb
@@ -5,4 +5,19 @@ class PubsController < ApplicationController
         render json: pubs
         # render plain: "hello world"
     end
+
+    def create
+        pub = Pub.new(pub_params)
+    
+        if pub.save
+          render json: pub, status: :created
+        else
+          render json: pub.errors, status: :unprocessable_entity
+        end
+      end
+
+    private
+    def pub_params
+    params.require(:pub).permit!
+    end
 end
